fb3d3eeac1d41f8712577b4abd3af44f023dfc54
ghpark <pkh879@gmail.com>
2023-02-18 20:07:17 +0900

    [FIX] 라이브러리 배포 후 경로 오류

--- src/deploy.js
+++ src/deploy.js
@@ -1,15 +1,15 @@
-import vuiCard from '@/components/cards/vuiCard.vue'
-import vuiTable from '@/components/table/vuiTable.vue'
-import vuiSelect from '@/components/inputs/vuiSelect.vue'
-import vuiSlide from '@/components/slide/vuiSlide.vue'
-import vuiEmpty from '@/components/etc/vuiEmpty.vue'
-import vuiLoading from '@/components/etc/vuiLoading.vue'
-import vuiPlayer from '@/components/video/vuiPlayer.vue'
-import vuiBreadcrumb from '@/components/etc/vuiBreadcrumb.vue'
-import vuiStepper from '@/components/stepper/vuiStepper.vue'
+import vuiCard from './components/cards/vuiCard.vue'
+import vuiTable from './components/table/vuiTable.vue'
+import vuiSelect from './components/inputs/vuiSelect.vue'
+import vuiSlide from './components/slide/vuiSlide.vue'
+import vuiEmpty from './components/etc/vuiEmpty.vue'
+import vuiLoading from './components/etc/vuiLoading.vue'
+import vuiPlayer from './components/video/vuiPlayer.vue'
+import vuiBreadcrumb from './components/etc/vuiBreadcrumb.vue'
+import vuiStepper from './components/stepper/vuiStepper.vue'
 
-import vuiHeader from '@/components/vuiHeader.vue'
-import vuiFooter from '@/components/vuiFooter.vue'
+import vuiHeader from './components/vuiHeader.vue'
+import vuiFooter from './components/vuiFooter.vue'
   
 export default {
   components: {
